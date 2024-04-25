fe8eff53be05d3e1ecec0a36391a5647baef182e
Yerlan <yerlan.newlife15@gmail.com>
2024-04-25 17:48:37 +0500

    MyStack

---
+++ src/MyStack.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+public class MyStack<T>{
+    private MyLinkedList stack;
+
+    public MyStack(){
+        this.stack = new MyLinkedList<>();
+    }
+
+    public void push(T item){
+        stack.addLast(item);
+    }
+
+    public T top(){
+        if(isEmpty()){
+            throw new RuntimeException("Stack is empty");
+        }
+        return (T)stack.getLast();
+    }
+
+    public boolean isEmpty(){
+        return stack.size() == 0;
+    }
+    public T pop(){
+        if(isEmpty()){
+            throw new RuntimeException("There is nothing to remove stack is empty");
+        }
+        T item = top();
+        stack.removeLast();
+        return item;
+    }
+
+    public int size(){
+        return stack.size();
+    }
+}
